677c6b425cda83685735ea5987851bd9b5ab3dd2
deepan <deepanappu1999@gmail.com>
2025-01-27 22:26:35 +0530

    adding header cpm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNgModule } from '../shared/prime-ng/prime-ng.module';
+import { HeaderComponent } from '../shared/components/header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,PrimeNgModule],
+  imports: [RouterOutlet,PrimeNgModule,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
