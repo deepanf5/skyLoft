ee5d9c72dde2bafbaff3690b3273ceabc4f162db
deepan <deepanappu1999@gmail.com>
2025-01-27 22:18:20 +0530

    imprt prime ng module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNgModule } from '../shared/prime-ng/prime-ng.module';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PrimeNgModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
