3abbcf1ada867c432cc4e3155d70c5d34a28b2c0
deepan <deepanappu1999@gmail.com>
2025-01-28 22:15:55 +0530

    chaning the route name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path:'dashboard',
+        path:'home',
         loadChildren: () => import('./../dashboard/dashboard-routing.module').then(r=> r.DashboardRoutingModule) // Lazy load the module
     },
     {
-        path:'',redirectTo:'dashboard',pathMatch:'full'
+        path:'',redirectTo:'home',pathMatch:'full'
     }
 ];
